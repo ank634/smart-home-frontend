6e4e5ccbbec53021ab56f40491d0150667766093
Emmanuel Bastidas <82973807+ank634@users.noreply.github.com>
2025-06-05 11:07:18 -0700

    Initial Commit

---
+++ smart_home_system_frontend/smart_home_system_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  smart_home_system_frontendApp.swift
+//  smart_home_system_frontend
+//
+//  Created by Emmanuel Bastidas on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct smart_home_system_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
