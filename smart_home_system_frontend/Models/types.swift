4c5b375015da8a3d1440035952614c36763911a3
Emmanuel Bastidas <82973807+ank634@users.noreply.github.com>
2025-06-30 08:16:32 -0700

    Custom types
    made custom types to abstract the smart home devices.

---
+++ smart_home_system_frontend/Models/types.swift
@@ -0,0 +1,19 @@
+//
+//  types.swift
+//  smart_home_system_frontend
+//
+//  Created by Emmanuel Bastidas on 6/6/25.
+//
+
+enum MdnsServiceType: String{
+    case http = "_http._tcp"
+    case mqtt = "_mqtt._tcp"
+    // dummy service remove
+    case light = "light"
+}
+
+enum DeviceType: String{
+    case light = "light"
+}
+
+
