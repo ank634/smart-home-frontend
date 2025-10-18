89bee78f5f69578868aa1535a1e1efbcdc31bba1
Emmanuel Bastidas <82973807+ank634@users.noreply.github.com>
2025-10-18 11:26:30 -0700

    implemented betterhome client
    Implemented client to allow crud functionality on deviecs, and rooms

--- smart_home_system_frontend/Client/BetterHome.swift
+++ smart_home_system_frontend/Client/BetterHome.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 import CocoaMQTT
+struct ProblemDetails: Codable{
+    var errorType: BetterHomeError
+    var title: String
+    var status: Int
+    var detail: String
+    
+    enum CodingKeys: String, CodingKey{
+        case errorType = "ErrorType"
+        case title = "Title"
+        case status = "Status"
+        case detail = "Detail"
+    }
+}
+
 class BetterHome{
     let mqttClient: CocoaMQTT5
     
@@ -15,17 +29,77 @@ class BetterHome{
         self.mqttClient = mqttClient
     }
     
-    func AddDevice(device: LightDto){
+    func AddDevice(device: LightDto) async throws{
+        guard let url = URL(string: "http://localhost:8080/iot-devices") else{
+            throw URLError(.badURL)
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = try JSONEncoder().encode(device)
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else{
+            throw URLError(.badServerResponse)
+        }
+        
+        if httpResponse.statusCode == 500{
+            throw URLError(.badServerResponse)
+        }
+        
+        if httpResponse.statusCode == 400{
+            var problemDetails = try JSONDecoder().decode(ProblemDetails.self, from: data)
+            throw problemDetails.errorType
+        }
         
     }
     
-    func EditDevice(deviceId: String, newName: String){
+    func EditDevice(deviceId: String, newName: String) async throws{
+        guard let url = URL(string: "http://localhost:8080/iot-devices/\(deviceId)") else{
+            throw URLError(.badURL)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
         
+        let body = ["DeviceName": newName]
+        request.httpBody = try JSONSerialization.data(withJSONObject: body)
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse else{
+            throw URLError(.badServerResponse)
+        }
+        
+        if httpResponse.statusCode == 500{
+            throw URLError(.badServerResponse)
+        }
+        
+        if httpResponse.statusCode == 400{
+            var problemDetails = try JSONDecoder().decode(ProblemDetails.self, from: data)
+            throw problemDetails.errorType
+        }
     }
     
     
-    func DeleteDevice(deviceId: String){
-        let URL = ""
+    func DeleteDevice(deviceId: String) async throws{
+        guard let url = URL(string: "http://localhost:8080/iot-devices/\(deviceId)") else{
+            throw URLError(.badURL)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        
+        let (_, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse else{
+            throw URLError(.badServerResponse)
+        }
+        
+        if httpResponse.statusCode == 500{
+            throw URLError(.badServerResponse)
+        }
+        
+        if httpResponse.statusCode == 404{
+            throw URLError(.fileDoesNotExist)
+        }
     }
     
     
@@ -41,47 +115,20 @@ class BetterHome{
             throw URLError(.badServerResponse)
         }
         
+        if httpResponse.statusCode == 500{
+            throw URLError(.badServerResponse)
+        }
+        
         let results = try JSONDecoder().decode([DeviceDto].self, from: data)
         
         for resultDto in results{
             switch resultDto{
                 case .lightDto(let lightDto):
                 fetchedDevices.append(.light(light: Light(mqttClient: mqttClient, lightDto: lightDto)))
+                default:
+                    print("Not implemented all switch cases")
             }
         }
         return fetchedDevices
-    }
-}
-
-enum Device: Hashable, Identifiable, Observable {
-    case light(light: Light)
-    
-    var id: Self{
-        self
-    }
-}
-
-enum DeviceDto: Codable{
-    case lightDto(lightDto: LightDto)
-    
-    enum CodingKeys: String, CodingKey{
-        case deviceType = "DeviceType"
-    }
-    
-    init(from decoder: any Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let type = try container.decode(DeviceType.self, forKey: CodingKeys.deviceType)
-        
-        switch type{
-        case .light:
-            self = .lightDto(lightDto: try LightDto(from: decoder))
-        }
-    }
-    
-    func encode(to encoder: any Encoder) throws {
-        switch self {
-        case .lightDto(let lightDto):
-            try lightDto.encode(to: encoder)
-        }
     }
 }
